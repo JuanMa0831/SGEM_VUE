79900c18eb91ed1cb21c1a08be1f797d8e6fbd2e
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-05-24 22:54:23 -0500

    IndexC_VUE

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Vue from 'vue';
-import Router from 'vue-router';
 import Conciertos from '../components/Conciertos.vue';
+
 
 const routes = [
   {
